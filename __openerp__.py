0440b3d30d0a7ae5fbd267245461c3fe4ff917e5
drohnet <drohne@gmail.com>
2017-06-10 11:30:35 +0200

    Ampliar las vistas

--- __openerp__.py
+++ __openerp__.py
@@ -4,4 +4,6 @@
     'description': 'Extend the To-Do app to multiuser.',
     'author': 'Daniel Reis',
     'depends': ['fundamentos_desarrollo_capitulo2'],
+    'data':[
+        'views/todo_view.xml'],
 }
